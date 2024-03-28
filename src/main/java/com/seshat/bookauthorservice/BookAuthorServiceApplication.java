8cca21da6f1b4370048d7d72844a9ea3c6851e94
Janith11 <janithpramuditha042@gmail.com>
2024-03-28 07:54:53 +0530

    first commit

---
+++ src/main/java/com/seshat/bookauthorservice/BookAuthorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seshat.bookauthorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAuthorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAuthorServiceApplication.class, args);
+	}
+
+}
